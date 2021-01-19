be24bf8f48a27c6565dd3b261cd0807e2a583b46
Gordon Yeong <gordon.yeong@broadsource.com.au>
2021-01-19 14:52:06 +1100

    was takign into account of the filler

--- lib/REPORT.pm
+++ lib/REPORT.pm
@@ -3,60 +3,91 @@ package REPORT;
 use strict;
 use warnings;
 
+use Readonly;
+use Data::Dumper;
+
+# CONFIGURATIONS
+
+Readonly my $max_input_characters_per_line => 302; 
+
 # configuration for input elements in the input file.
 # the value of the hash ref represents the length of the element's string
+# Assumption: There must only be one slice per hashref
 
-my %input_element_placements = (
-	'RECORD CODE' => 3,
-	'CLIENT TYPE' => 4,
-	'CLIENT NUMBER' => 4,
-	'ACCOUNT NUMBER' => 4,
-	'SUBACCOUNT NUMBER' => 4,
-	'OPPOSITE PARTY CODE' =>6,
-        'PRODUCT GROUP CODE' => 2,
-	'EXCHANGE CODE' => 4,
-	'SYMBOL' =>6,
-	'EXPIRATION DATE' => 8,
-	'CURRENCY CODE' => 3,
-	'MOVEMENT CODE' => 2,
-	'BUY SELL CODE' => 1,
-	'QUANTITY LONG SIGN'=> 1,
-	'QUANTITY LONG' => 10,
-	'QUANTITY SHORT SIGN' => 1,
-	'QUANTITY SHORT' => 10,
-	'EXCH/BROKER FEE / DEC' => 12,
-	'EXCH/BROKER FEE DC' => 1,
-	'EXCH/BROKER FEE CUR CODE' => 3,
-	'CLEARING FEE / DEC' => 12,
-	'CLEARING FEE D C' => 1,
-	'CLEARING FEE CUR CODE' => 3,
-	'COMMISSION' => 12,
-	'COMMISSION D C' => 1,
-	'COMMISSION CUR CODE' => 3,
-	'TRANSACTION DATE' => 8,
-	'FUTURE REFERENCE' => 6,
-	'TICKET NUMBER' => 6,
-	'EXTERNAL NUMBER' => 6,
-	'TRANSACTION PRICE  / DEC' => 15,
-	'TRADER INITIALS' => 6,
-	'OPPOSITE TRADER ID' => 7,
-	'OPEN CLOSE CODE' => 1,
-	'FILLER' => 127,
+Readonly::Array my @input_element_placements => (
+    { 'RECORD CODE'              => 3, },
+    { 'CLIENT TYPE'              => 4, },
+    { 'CLIENT NUMBER'            => 4, },
+    { 'ACCOUNT NUMBER'           => 4, },
+    { 'SUBACCOUNT NUMBER'        => 4, },
+    { 'OPPOSITE PARTY CODE'      => 6, },
+    { 'PRODUCT GROUP CODE'       => 2, },
+    { 'EXCHANGE CODE'            => 4, },
+    { 'SYMBOL'                   => 6, },
+    { 'EXPIRATION DATE'          => 8, },
+    { 'CURRENCY CODE'            => 3, },
+    { 'MOVEMENT CODE'            => 2, },
+    { 'BUY SELL CODE'            => 1, },
+    { 'QUANTITY LONG SIGN'       => 1, },
+    { 'QUANTITY LONG'            => 10, },
+    { 'QUANTITY SHORT SIGN'      => 1, },
+    { 'QUANTITY SHORT'           => 10, },
+    { 'EXCH/BROKER FEE / DEC'    => 12, },
+    { 'EXCH/BROKER FEE DC'       => 1, },
+    { 'EXCH/BROKER FEE CUR CODE' => 3, },
+    { 'CLEARING FEE / DEC'       => 12, },
+    { 'CLEARING FEE D C'         => 1, },
+    { 'CLEARING FEE CUR CODE'    => 3, },
+    { 'COMMISSION'               => 12, },
+    { 'COMMISSION D C'           => 1, },
+    { 'COMMISSION CUR CODE'      => 3, },
+    { 'TRANSACTION DATE'         => 8, },
+    { 'FUTURE REFERENCE'         => 6, },
+    { 'TICKET NUMBER'            => 6, },
+    { 'EXTERNAL NUMBER'          => 6, },
+    { 'TRANSACTION PRICE  / DEC' => 15, },
+    { 'TRADER INITIALS'          => 6, },
+    { 'OPPOSITE TRADER ID'       => 7, },
+    { 'OPEN CLOSE CODE'          => 1, },
+#    { 'FILLER'                   => 127, },
 );
 
-
-
 # methods
-
-
 sub identify_transaction_elements
 {
 	my ($transaction_line) = @_;
+	my @raw_data = split qr{}, $transaction_line, $max_input_characters_per_line;
 
-	my @raw_data = split qr{}, $r;
+    # Sanitise the data
+	@raw_data = map { defined $_ and $_ =~ m{\w} ? $_ : q{} } @raw_data[ 0 .. $max_input_characters_per_line];
 
+	print STDERR qq{Raw DATA is } . Dumper(@raw_data);
+
+	my %data = ();
+	my $index = 0;
+	foreach my $element_config (@input_element_placements)
+	{
+		ELEMENT_NAME: foreach my $element_name (keys %{$element_config})
+		{
+			my $ending_index = $index + $element_config->{$element_name} - 1;
+			print qq{index $index, Ending "$ending_index"\n};
+			my @raw_selections = $raw_data[ $index .. $ending_index];
+			#foreach my $f (  $index .. $ending_index )
+			#{
+				#print STDERR qq{ \t$f has "} . $raw_data[$f] . q{"\n};
+			#}
+			$data{$element_name} = join q{}, $raw_data[ $index .. $ending_index];
+			print qq{$element_name -> } . $data{$element_name} . qq{...\n};
+
+			$index = $ending_index+1;
+			last ELEMENT_NAME;
+		}
+	}
  
+	#print qq{Result: } . Dumper(%data);
 }
+
+
 
 sub generate_report_contents
 {
