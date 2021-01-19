30dd792b76c066b70132ac0fb2b03c657d7a1d24
Gordon Yeong <gordon.yeong@broadsource.com.au>
2021-01-19 17:39:37 +1100

    needs test and clean p

--- generate_daily_summary_report.pl
+++ generate_daily_summary_report.pl
@@ -10,26 +10,40 @@ use IO::File;
 use Data::Dumper;
 
 my $help = undef;
+
+
 my $input_file = undef;
- 
-      GetOptions (
+GetOptions (
                   "input_file=s"   => \$input_file,
                   "help"  => \$help)
-      or die("Error in command line arguments\n");
+or die("Error in command line arguments\n");
 
-unless (defined($input_file))
+my %file = (
+    'input'  => q{input.csv},
+    'output' => q{output.csv},
+);
+
+if (defined($input_file))
 {
-    die qq{No input file supplied to daily summary report generator.\n};
+    $file{'input'} = $input_file;
+}
+else{
+    print q{No input file supplied to daily summary report generator. Will use}
+    . qq{ the default file, }
+    . $file->{'input'}
+    . qq{\n}
+    ;
 }
 
-my $fh = IO::File->new($input_file, q{r});
+my $fh = IO::File->new($file{'input'}, q{r});
 
 unless (defined $fh) {
-    die qq{Cannot open file, $input_file: $!};
+    die q{Cannot open file, }. $file{'input'} . qq{: $!};
 }
 
-
 my @parsed_data = ();
+eval
+{
 # parse all data in each of the input line to recognisable
 # elements. The identifies the unique sets of client and product
 # information
@@ -38,8 +52,11 @@ while (<$fh>)
     my %elements = REPORT::identify_transaction_elements($_);
     push @parsed_data, \%elements;
 }
+undef $fh;
 
 my %summary_data = REPORT::get_summary_data(@parsed_data);
+
+REPORT::generate_csv_report($output_file_name);
 
 
 use Data::Dumper;
@@ -47,8 +64,9 @@ print qq{ Summary is } . Dumper(\%summary_data);
  #   my @report_rows = REPORT::generate_report_contents(\%elements);
 #	print qq{REPORT: } . Dumper(@report_rows) . qq{\n};
 
-
-
-    undef $fh;
-
+};
+if ($@)
+{
+    print STDERR qq{Exception trapped: $@};
+}
 1;
